489f058a75709e5325068800ad718eed300dedb3
Siddhant Kashyap <86836609+Siddhant-Kashyap@users.noreply.github.com>
2024-05-03 02:16:22 +0530

    basic structure Landing page

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,17 @@
+const LandingPage = () => {
+  return (
+    <>
+      <div className="main">
+        <div className="filters">
+          {/* Filters  */}
+        </div>
+
+        <div className="job-card">
+         {/* JobList card */}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
